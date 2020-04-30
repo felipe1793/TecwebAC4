679693e68f995030a8a1e80c4a341e122d99e805
felipe1793 <felipe1793@hotmail.com>
2020-04-29 21:15:35 -0300

    gfh

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> Hello, World!</h1>'
+    return render_template
